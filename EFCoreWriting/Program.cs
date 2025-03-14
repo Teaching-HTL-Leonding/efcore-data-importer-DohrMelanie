fee7f7f474df51b294541210ab9864bc038b6ba7
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-03-14 08:43:08 +0100

    added task 1

---
+++ EFCoreWriting/Program.cs
@@ -0,0 +1,12 @@
+﻿using EFCoreWriting;
+
+var factory = new ApplicationDataContextFactory();
+var context = factory.CreateDbContext(args: []);
+
+var customer = new Customer(0, "Hogwarts", "UK", "Scotland");
+
+context.Customers.Add(customer);
+await context.SaveChangesAsync();
+
+customer.CompanyName += " School of Witchcraft and Wizardry";
+await context.SaveChangesAsync();
